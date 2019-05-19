2db21aa8417a8ebc485da6a05880dea8e767d3d9
BohdanRadchenko <radchenkobs@gmail.com>
2019-05-19 21:43:21 +0300

    parallax

--- project-3/project-3/js/main.js
+++ project-3/project-3/js/main.js
@@ -1,1 +1,10 @@
-'use strict';
+'use stict'
+
+function Parallax(event) {
+    this.querySelectorAll('.layer').forEach(layer => {
+        let speed = layer.getAttribute('data-speed')
+        layer.style.transform = `translateX(${event.clientX*speed/1000}px)`;
+    });
+}
+
+document.addEventListener('mousemove',Parallax);
