66a056067789406e49ddfb2bc517d1009fbd1aef
BohdanRadchenko <radchenkobs@gmail.com>
2019-05-20 10:23:39 +0300

    set 3d transform

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -12,4 +12,23 @@ function progressBar(e) {
 
     progress.style.width = per + '%';
 }
+/*3d card transform*/
+const cards = document.querySelectorAll('.card');
 
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('mousemove',startRotate);
+    card.addEventListener('mouseout',stopRotate);
+}
+
+function startRotate(event) {
+    const cardItem = this.querySelector('.card__item');
+    const halfHieight = cardItem.offsetHeight / 2;
+    const halfWidth = cardItem.offsetWidth / 2;
+    cardItem.style.transform = 'rotateX('+ -(event.offsetY - halfHieight)/4 +'deg) rotateY('+(event.offsetX - halfWidth)/4+'deg)';
+}
+
+function stopRotate(event) {
+    const cardItem = this.querySelector('.card__item');
+    cardItem.style.transform = 'rotate(0)';
+}
